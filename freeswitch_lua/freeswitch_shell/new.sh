b6726a10fee611437ce09bba97f9cac4d95048b0
Mr han <849272199@qq.com>
2019-08-08 09:53:18 +0800

    xml配置

--- freeswitch_lua/freeswitch_shell/new.sh
+++ freeswitch_lua/freeswitch_shell/new.sh
@@ -1,11 +1,11 @@
-###number####
-###passwd####
 ###passwd和number长度大小相等
 ###配置多个xml文件
-for i in `cat number`; do sed -e s/10086/$i/g 10086.xml > $i.xml ; done
+##for i in `cat number`; do sed -e s/10086/$i/g 10086.xml > $i.xml ; done
 ###修改每个xml的密码
-
+##example.xml
 #!/bin/bash
+passwd=`cat $1.xml|grep password|awk '{print $3}'|cut -d = -f2| tr -d -c "[a-zA-z0-9]\n"`
+for i in `cat number`; do sed -e s/$1/$i/g $1.xml > $i.xml ; done
 b=1
 c=1
 for j in `cat passwd`; do
@@ -13,6 +13,6 @@ for j in `cat passwd`; do
         let b++
 done
 for i in `cat number`; do
-        sed -i s/wedd86/${array[$c]}/g $i.xml
+        sed -i s/${passwd}/${array[$c]}/g $i.xml
         let c++
 done
